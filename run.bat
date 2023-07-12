5aa132e8c30436283a563e5e522bc9419ff66d77
nsierra <nicofsierra@gmail.com>
2023-07-12 20:35:02 -0300

    fix run

--- run.bat
+++ run.bat
@@ -1,10 +1,14 @@
 PATH=C:\TASM;
 
 tasm numbers.asm
-tasm /m2 final.asm
+tasm number.asm
+tasm macros2.asm
+tasm final.asm
 tlink final.obj numbers.obj
-ejemplo.exe
+final.exe
 del final.obj 
 del numbers.obj 
+del number.obj 
+del macros2.obj 
 del final.exe
 del final.map
